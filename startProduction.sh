7b5ffc2716a5e18818671772d10615c38dfefb55
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2024-07-02 15:50:16 +0200

    correct secret key

--- startProduction.sh
+++ startProduction.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-env_name=Demonstrator
+env_name=dtap
 
 if [ ! -f ./.secret_key ]
 then
@@ -10,7 +10,7 @@ fi
 if [ -z "$STY" ]
 then
     # we are not running in screen
-    exec screen -dm -S ${env_name} -L -Logfile labspace_$(date '+%d_%m_%Y_%H_%M_%S').log /bin/bash "$0";
+    exec screen -dm -S ${env_name} -L -Logfile dtap_$(date '+%d_%m_%Y_%H_%M_%S').log /bin/bash "$0";
     
 else
     # we are running in screen, provide commands to execute
